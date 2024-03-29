0cc274e3e40ffbf1798bb99ca845d5c59394972d
Nikolas Luiz Schmitt <nikolas.luiz.schmitt@gmail.com>
2024-03-29 10:15:57 -0300

    chore: documentação da classe Shape

--- draw/shapes/shape.py
+++ draw/shapes/shape.py
@@ -6,8 +6,20 @@ from config.configurator import Configurator
 
 
 class Shape(ABC):
+    """
+        Classe que define uma forma do jogo que será gerada e deverá ser
+        encaixada.
+    """
 
     def __init__(self, configurator: Configurator, speed_movimentation_y: float, size: float):
+        """
+            Construtor com os parâmetros da forma.
+
+            :param configurator: Classe de configuração do jogo
+            :param speed_movimentation_y: Velocidade que o objeto deverá se movimentar no eixo Y
+            :param size: Tamanho do objeto
+        """
+
         self.position_x = configurator.screen_width // 2
         self.position_y = configurator.screen_height + 50
         self.speed_movimentation_y = speed_movimentation_y
@@ -16,4 +28,8 @@ class Shape(ABC):
 
     @abstractmethod
     def draw(self):
+        """
+            Função responsável por desenhar a forma.
+        """
+
         pass
